ff81c4ec183362e70fc4bfb35bd76c29dd49cb57
A13CH <calechoelscher@gmail.com>
2025-03-13 12:48:09 -0700

    removed local call from data tests

--- tests/unit/test_currency_converter.py
+++ tests/unit/test_currency_converter.py
@@ -7,7 +7,7 @@ Description: Test cases for the currency_converter program
 
 import streamlit as st
 from requests import get
-from app.data import get_data, get_currency_list
+from data import get_data, get_currency_list
 
 key = st.secrets["API_key"]
 
@@ -19,5 +19,5 @@ def test_get_data() -> None:
 def test_get_currency_list() -> None:
     """Testing get_currency_list function"""
     currency_list = get_currency_list()
-    assert currency_list == get(f"https://api.exchangeratesapi.io/v1/symbols?access_key=c{key}", timeout=3).json()
+    assert currency_list == get(f"https://api.exchangeratesapi.io/v1/symbols?access_key={key}", timeout=3).json()
 
